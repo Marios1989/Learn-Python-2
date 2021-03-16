bdeda5bfed701fe1c41f3117b5ae37248d2d8ad5
Marios1989 <68182435+Marios1989@users.noreply.github.com>
2021-03-16 18:13:23 +0200

    Add files via upload

---
+++ Learn Python 2.py
@@ -0,0 +1,85 @@
+#import print statement
+"Hello Marios"
+print("Hello Marios")
+
+#Handling errors
+"How do you make a hot dog stand?"
+"You take away its chair!"
+
+#import variables
+todays_date = "March 16, 2021"
+
+#import arithmetics
+quotient = 18/9
+product = 2
+
+#Updating Variables
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+annual_rainfall = january_to_june_rainfall
+
+july_rainfall = 1.05
+annual_rainfall += july_rainfall
+
+august_rainfall = 4.91
+annual_rainfall += august_rainfall
+
+september_rainfall = 5.16
+annual_rainfall += september_rainfall
+
+october_rainfall = 7.20
+annual_rainfall += october_rainfall
+
+november_rainfall = 5.06
+annual_rainfall += november_rainfall
+
+december_rainfall = 4.06
+annual_rainfall += december_rainfall
+
+#Add comments
+city_name = "Moscow"
+#population
+city_pop = 1300000
+
+#Numbers
+cucumbers = 3
+price_per_cucumber = 3.25
+total_cost_ = cucumbers * price_per_cucumber
+print( total_cost_)
+
+#Division types
+cucumbers = 100
+num_people = 6
+whole_cucumbers_per_person = cucumbers / num_people
+print  (whole_cucumbers_per_person)
+float_cucumbers_per_person = float (cucumbers) / num_people
+print (float_cucumbers_per_person)
+
+#Multi-line Strings
+haiku = """The old pond,
+A frog jumps in:
+Plop!"""
+
+print (haiku)
+
+#Booleans
+# Hi! I'm Maria and I live in script.py.
+# I'm an expert Python coder.
+# I'm 21 years old and I plan to program cool stuff forever.
+age_is_12 = False
+name_is_maria = True
+
+#Value Errors
+float_1 = 0.25
+float_2 = 40.0
+#product = float_1 * float_2
+print(product)
+big_string = "The product was 10"
+
+#Review
+skill_completed = "Python Syntax"
+exercises_completed = 13
+#The amount of points for each exercise may change, because points don't exist yet
+points_per_exercise = 5
+point_total = 100
+point_total += exercises_completed * points_per_exercise
+print ("I got "+str(point_total)+" points!")
